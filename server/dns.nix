4febde0c4e4e8c4b08ff97904a70c747300b8d16
David Gatti <dawid@gatti.io>
2024-11-09 16:13:46 +0100

    Update dns.nix

--- server/dns.nix
+++ server/dns.nix
@@ -1,21 +1,15 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  unstable = import <nixos-unstable> { };
-in
 {
-  imports = [
-    /etc/nixos/hardware-configuration.nix
-  ];
+  imports =
+    [ 
+      /etc/nixos/hardware-configuration.nix 
+    ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  nixpkgs.config.allowUnfree = true;
-
   system.stateVersion = "24.05";
-  networking.hostName = "HomeLab";
+  networking.hostName = "PiHole";
 
   services.openssh = {
     enable = true;
@@ -27,112 +21,39 @@ in
 
   users.users.nixos = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "video" "render" ];
+    extraGroups = [ "wheel" "docker" "nixos" ];
     password = "nixos";
   };
 
   environment.systemPackages = with pkgs; [
-    home-manager
-    code-server
     docker
-    nixpkgs-fmt
-    cifs-utils
-    rocm-opencl-icd
-    rocmPackages.rocminfo
   ];
-
-  services.code-server = {
-    enable = true;
-    user = "nixos";
-    port = 8080;
-    host = "0.0.0.0";
-  };
-
-  services.ollama = {
-    package = unstable.ollama;
-    enable = true;
-    acceleration = "rocm"; # or "cuda"
-  };
 
   # Enable Docker with default configuration only
   virtualisation.docker.enable = true;
 
-  # Enable Bluetooth hardware support
-  hardware.bluetooth.enable = true;
-
-  # Optional: Docker-based Home Assistant service
-  systemd.services.install-homeassistant = {
-    description = "Install and run Home Assistant in Docker";
+  systemd.services.install-pihole = {
+    description = "Install and run Pi-hole in Docker";
     after = [ "docker.service" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStartPre = [
-        "-${pkgs.docker}/bin/docker stop homeassistant || true"
-        "-${pkgs.docker}/bin/docker rm homeassistant || true"
+        "-${pkgs.docker}/bin/docker stop pihole"
+        "-${pkgs.docker}/bin/docker rm pihole"
       ];
-      ExecStart = "${pkgs.docker}/bin/docker run -d --name homeassistant "
-        + "-p 8123:8123 "
+      ExecStart = "${pkgs.docker}/bin/docker run -d --name pihole "
         + "--net=host "
         + "-e TZ=\"Europe/Rome\" "
-        + "-v /etc/homeassistant:/config "
-        + "-v /run/dbus:/run/dbus:ro "
-        + "-v /mnt/music:/media/music:rw "
-        + "homeassistant/home-assistant:latest";
+        + "-e WEBPASSWORD=\"password\" "
+        + "-e DNSMASQ_LISTENING=\"all\" "
+        + "-e DNSSEC=\"true\" "
+        + "-e DNS_FQDN_REQUIRED=\"true\" "
+        + "-e DNS_BOGUS_PRIV=\"true\" "
+        + "-v /etc/pihole:/etc/pihole "
+        + "-v /etc/dnsmasq.d:/etc/dnsmasq.d "
+        + "pihole/pihole";
     };
   };
-
-  systemd.services.jellyfin = {
-    description = "Jellyfin Media Server";
-    after = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "simple";
-
-      # Pre-start commands to ensure volumes are created
-      ExecStartPre = [
-        # Ensure volume for config is created
-        "${pkgs.docker}/bin/docker volume create jellyfin-config"
-
-        # Ensure volume for cache is created
-        "${pkgs.docker}/bin/docker volume create jellyfin-cache"
-
-        # Stop and remove existing container if it exists
-        "-${pkgs.docker}/bin/docker stop jellyfin || true"
-        "-${pkgs.docker}/bin/docker rm jellyfin || true"
-      ];
-
-      # Start the Jellyfin container with volume mounts
-      ExecStart = ''
-        ${pkgs.docker}/bin/docker run -d --name jellyfin \
-          -p 8096:8096 \
-          --restart always \
-          -v /mnt/media:/media:ro \
-          -v jellyfin-config:/config \
-          -v jellyfin-cache:/cache \
-          jellyfin/jellyfin:latest
-      '';
-    };
-  };
-
-  fileSystems."/mnt/media" = {
-    device = "//192.168.2.2/media";
-    fsType = "cifs";
-    options = [ "username=media" "password=PASSWORD" "rw" ];
-  };
-
-  fileSystems."/mnt/dropbox" = {
-    device = "//192.168.2.2/dropbox";
-    fsType = "cifs";
-    options = [
-      "guest"
-      "uid=1000"
-      "gid=100"
-      "file_mode=0644"
-      "dir_mode=0755"
-      "rw"
-      "vers=3.0"
-    ];
-  };
-
+  
 }
