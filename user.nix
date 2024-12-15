5da19f63582d57ecb7cce5d5c2b156d6455bf073
David Gatti <dawid@gatti.io>
2024-12-15 11:04:00 +0100

    cleanup

--- user.nix
+++ user.nix
@@ -1,66 +1,87 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "nixos";  # Replace with your actual username
-  home.homeDirectory = "/home/nixos";  # Adjust if your home directory is different
+    home.username = "nixos";
+    home.homeDirectory = "/home/nixos";
 
-  # Declare Home Manager state version
-  home.stateVersion = "23.05";  # Match the release of Home Manager you installed
+    # Declare Home Manager state version
+    home.stateVersion = "23.05";
 
-  # Enable Bash autocompletion for commands
-  programs.bash.enableCompletion = true;
+    # Enable Bash autocompletion for commands
+    programs.bash.enableCompletion = true;
 
-  # Enable and configure Git
-  programs.git = {
-    enable = true;
-    userName = "davidgatti";  # Replace with your actual name
-    userEmail = "411114+davidgatti@users.noreply.github.com";  # Replace with your actual email
-  };
-
-  # Add a few system packages for the user
-  home.packages = with pkgs; [
-    tree    # Directory listing in tree format
-    gh
-    btop
-    git
-    cmatrix
-    fzf
-    neofetch
-    mesa-demos
-    radeontop
-    handbrake
-
-    # Tools
-    mc
-    zip
-    pv
-
-    # Games
-    bastet
-    nudoku
-    
-    # Fun
-    asciiquarium
-    sl
-    tty-clock
-    nyancat
-  ];
-
-  # Enable Bash and customize the prompt
-  programs.bash = {
-    enable = true;
-
-    # Set up custom aliases
-    shellAliases = {
-      ll = "ls -alh";  # Convenient alias for directory listing
-      ls = "ls --color=auto";
-      search = "fzf";
-      system_info = "neofetch";
+    # Enable and configure Git
+    programs.git = {
+        enable = true;
+        userName = "davidgatti";
+        userEmail = "411114+davidgatti@users.noreply.github.com";
     };
 
-    # Customize the Bash prompt
-    initExtra = ''
-      export PS1="\[\e[35m\][\u@\h \A] \[\e[33m\]\w\[\e[0m\]\$ "
-    '';
-  };
+    # Add a few system packages for the user
+    home.packages = with pkgs; [
+        tree
+        gh
+        btop
+        git
+        cmatrix
+        fzf
+        neofetch
+        mesa-demos
+        radeontop
+        handbrake
+
+        # Tools
+        mc
+        zip
+        pv
+
+        # Games
+        bastet
+        nudoku
+
+        # Fun
+        asciiquarium
+        sl
+        tty-clock
+        nyancat
+    ];
+
+    # Enable Bash and customize the prompt
+    programs.bash = {
+        enable = true;
+      
+        # Set up custom aliases
+        shellAliases = {
+            ll = "ls -alh";                 # Convenient alias for directory listing
+            ls = "ls --color=auto";         # Auto color for ls
+            search = "fzf";                 # Fuzzy finder
+            system_info = "neofetch";       # System info utility
+        };
+      
+        # Add custom configurations and export variables
+        initExtra = ''
+            # History configurations
+            export HISTFILESIZE=100000
+            export HISTSIZE=10000
+            shopt -s histappend
+            shopt -s checkwinsize
+            shopt -s extglob
+            shopt -s globstar
+            shopt -s checkjobs
+        
+            # Download and source git-prompt.sh if not present
+            if [ ! -f ~/.git-prompt.sh ]; then
+            curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+            fi
+
+            source ~/.git-prompt.sh
+        
+            # Set options for Git prompt
+            export GIT_PS1_SHOWDIRTYSTATE=1
+            export GIT_PS1_SHOWUNTRACKEDFILES=1
+        
+            # Customize the bash prompt
+            export PS1="\[\e[0;30;48;2;255;0;0m\] \u][\W  $(__git_ps1 '%s')\[\e[0m\]\[\e[38;2;255;0;0m\]\[\e[0m\] "
+        '';
+      };      
 }
