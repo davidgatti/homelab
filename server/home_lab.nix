5864f41fc3de5b47bd2a6867a2b6e6e7a8dab6e6
David Gatti <dawid@gatti.io>
2024-12-25 09:54:08 +0100

    Update home_lab.nix

--- server/home_lab.nix
+++ server/home_lab.nix
@@ -88,6 +88,8 @@
                 -v /etc/homeassistant:/config \
                 -v /run/dbus:/run/dbus:ro \
                 -v /mnt/music:/media/music:rw \
+                --device=/dev/bus/usb \
+                --privileged \
                 homeassistant/home-assistant:latest
             '';
         };
@@ -120,10 +122,86 @@
         };
     };
 
+    systemd.services.node-red = {
+        description = "Node-RED Service";
+        after = [ "docker.service" "docker-macvlan.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStartPre = [
+                "${pkgs.docker}/bin/docker volume create nodered-data"
+                "-${pkgs.docker}/bin/docker stop node-red || true"
+                "-${pkgs.docker}/bin/docker rm node-red || true"
+            ];
+            ExecStart = ''
+                ${pkgs.docker}/bin/docker run -d --name node-red \
+                --network=home_bridge \
+                --ip=192.168.2.16 \
+                --hostname node-red \
+                --mac-address B8:27:EB:12:34:58 \
+                -e TZ="Europe/Rome" \
+                -e PORT=80 \
+                -v nodered-data:/data:rw \
+                nodered/node-red:latest
+            '';
+        };
+    };
+
+    systemd.services.n8n = {
+        description = "n8n Service";
+        after = [ "docker.service" "docker-macvlan.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStartPre = [
+                "${pkgs.docker}/bin/docker volume create n8n-data"
+                "-${pkgs.docker}/bin/docker stop n8n || true"
+                "-${pkgs.docker}/bin/docker rm n8n || true"
+            ];
+            ExecStart = ''
+                ${pkgs.docker}/bin/docker run -d --name n8n \
+                --network=home_bridge \
+                --ip=192.168.2.17 \
+                --hostname n8n \
+                --mac-address B8:27:EB:12:34:59 \
+                -e N8N_SECURE_COOKIE=false \
+                -e TZ="Europe/Rome" \
+                -e N8N_PORT=80 \
+                -v n8n-data:/home/node/.n8n:rw \
+                n8nio/n8n:latest
+            '';
+        };
+    };
+
+    systemd.services.qdrant = {
+        description = "Qdrant Vector Database Service";
+        after = [ "docker.service" "docker-macvlan.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStartPre = [
+                "${pkgs.docker}/bin/docker volume create qdrant-data"
+                "-${pkgs.docker}/bin/docker stop qdrant || true"
+                "-${pkgs.docker}/bin/docker rm qdrant || true"
+            ];
+            ExecStart = ''
+                ${pkgs.docker}/bin/docker run -d --name qdrant \
+                --network=home_bridge \
+                --ip=192.168.2.18 \
+                --hostname qdrant \
+                --mac-address B8:27:EB:12:34:60 \
+                -e TZ="Europe/Rome" \
+                -v qdrant-data:/qdrant/storage:rw \
+                qdrant/qdrant
+            '';
+        };
+    };
+
+
     fileSystems."/mnt/media" = {
         device = "//192.168.2.2/media";
         fsType = "cifs";
-        options =[ "username=media""password=PASSWORD""rw" ];
+        options =[ "username=media""password=*vL9wax8!HeBM3BoJ9*z@RR4N7oP2NKCPb7ybQhVhnavakXrJs""rw" ];
     };
 
     fileSystems."/mnt/dropbox" = {
